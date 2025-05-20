458affd2fa7f0f86332bfed626223cd7de075c35
ziyoviddin <ziyoviddint63@gmail.com>
2025-05-20 14:41:40 +0500

    sevice

--- src/components/freeAdd/FreeAdd.jsx
+++ src/components/freeAdd/FreeAdd.jsx
@@ -5,17 +5,17 @@ function FreeAdd() {
     <>
           <div className="free_cards">
             <div className="free_card">
-              <img src="/public/imgs/Services.png" alt="" />
+              <img src="/imgs/Services.png" alt="" />
               <h3>FREE AND FAST DELIVERY</h3>
               <p>Free delivery for all orders over $140</p>
             </div>
             <div className="free_card">
-              <img src="/public/imgs/Services (1).png" alt="" />
+              <img src="/imgs/Services1.png" alt="" />
               <h3>24/7 CUSTOMER SERVICE</h3>
               <p>Friendly 24/7 customer support</p>
             </div>
             <div className="free_card">
-              <img src="/public/imgs/Services (2).png" alt="" />
+              <img src="/imgs/Services2.png" alt="" />
               <h3>MONEY BACK GUARANTEE</h3>
               <p>We reurn money within 30 days</p>
             </div>
